7519042d21e69111cb1dfb01d79fe57819625684
JuaniV013 <112590165+JuaniV013@users.noreply.github.com>
2023-07-06 11:25:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var loader = document.querySelector('.croissant');
-  var background = document.querySelector('.back');
-  var nav = document.querySelector('nav');
-  var produccionLink = document.querySelector('a[href="Produccion.html"]');
-
-  // Verifica si la página anterior fue la página de producción
-  var previousPage = sessionStorage.getItem('previousPage');
-  if (previousPage === 'Produccion.html') {
-    // Muestra el croissant y oculta la página anterior
+    var loader = document.querySelector('.croissant');
+    var background = document.querySelector('.back');
+    var nav = document.querySelector('nav');
+    var produccionLink = document.querySelector('a[href="Produccion.html"]');
+  
     background.style.background = 'none'; 
     loader.style.display = 'block';
     nav.style.display = 'none';
-  }
-
-  produccionLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Evita que se siga el enlace de forma predeterminada
-    
-    // Realiza la animación antes de redireccionar
-    background.style.background = 'none'; 
-    loader.style.display = 'block';
-    nav.style.display = 'none';
-
+  
+    produccionLink.addEventListener('click', function(event) {
+      event.preventDefault(); // Evita que se siga el enlace de forma predeterminada
+      
+      // Realiza la animación antes de redireccionar
+      background.style.background = 'none'; 
+      loader.style.display = 'block'
+      nav.style.display = 'none'
+  
+      setTimeout(function() {
+        // Redirecciona a la página de producción
+        background.style.background = 'url("Mordiscologo.png") no-repeat top center #202325';
+        window.location.href = produccionLink.href;
+      }, 3000); // Espera 1 segundo antes de redireccionar (ajusta el tiempo según sea necesario)
+    });
+  
     setTimeout(function() {
-      // Redirecciona a la página de producción
-      sessionStorage.setItem('previousPage', 'Produccion.html');
-      window.location.href = produccionLink.href;
-    }, 3000); // Espera 3 segundos antes de redireccionar (ajusta el tiempo según sea necesario)
+      background.style.background = 'url("Mordiscologo.png") no-repeat center center #202325';
+      loader.style.display = 'none';
+      nav.style.display = 'block';
+    }, 5000);
   });
-
+  
   window.addEventListener('unload', function() {
     // Detener la animación del croissant
     var croissant = document.querySelector('.croissant');
     croissant.style.animationPlayState = 'paused';
   });
-});
-
+  
 
